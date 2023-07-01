1c2e06a13df13f43092bb3ac53cb2ba50baa8f88
Mahesh <chaudharymahesh076@gmail.com>
2023-07-01 11:29:28 +0530

    Mahesh added successfully

---
+++ Employe.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Employee
+{
+  public static void main(String args[])
+{
+   Scanner sc=new Scanner(System.in);
+   String str[]={"Mahesh","Anand","Arpita","Deepak"};
+   for(String i:str)
+{
+   System.out.print(i+" ");
+ }
+}
+} 
